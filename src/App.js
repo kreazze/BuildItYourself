25a671a2a55dc974220472bfda2310c6714ce8c9
ronniechua01 <sjpchua@iskolarngbayan.pup.edu.ph>
2022-01-11 03:56:53 +0800

    New commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi <code>src/App.js</code> and save to hi.
+          Hello hi hello musta <code>src/App.js</code> and save to hi.
         </p>
         <a
           className="App-link"
