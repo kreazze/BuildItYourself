3e7bc2efd54e195504b04410a44eb19556c3af31
Kris Anne Del Prado <krisannedelprado@gmail.com>
2022-01-19 00:32:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/Navbar';
+import NavBar from './Components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Pages'
 
