e554eb9cc8f28bc7d5439079e7d28dc656e6c6cf
ronniechua01 <sjpchua@iskolarngbayan.pup.edu.ph>
2022-01-18 23:15:15 +0800

    fixed routers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/Navbar';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Pages'
 
 function App() {
   return (
     <Router>
       <NavBar></NavBar>
+      <Switch>
+        <Route path ='/' exact component = {Home} />
+      </Switch>
     </Router>
   );
 }
