b7d97e8e9156a6c4556dc8c7e9db4f9f8f8fbfe3
ronniechua01 <sjpchua@iskolarngbayan.pup.edu.ph>
2022-02-16 03:41:06 +0800

    extra backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express()
 const mongoose = require('mongoose')
 const OrdersModel = require('./models/Orders')
 
+const cors = require('cors')
+
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect("mongodb+srv://ronniechua01:ronniechua01@customerinfo.hckkc.mongodb.net/Customer?retryWrites=true&w=majority")
 
