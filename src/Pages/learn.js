84da572aa9a0d74ad30b709a82f5f801ae47cf09
ronniechua01 <sjpchua@iskolarngbayan.pup.edu.ph>
2022-02-15 17:18:28 +0800

    new

---
+++ src/Pages/learn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Learn = () => {
+    return (
+        <div>
+            <div className='learnmore_div1'>
+                <h1>Learn Now!</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default Learn
