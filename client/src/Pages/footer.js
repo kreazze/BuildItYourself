38d23da4c06c82b2e3f053fea19a10e156ca932c
Kris Anne Del Prado <krisannedelprado@gmail.com>
2022-02-17 17:50:32 +0800

    Added footer

---
+++ client/src/Pages/footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './footer.css';
+import {BsFacebook, BsInstagram, BsYoutube} from 'react-icons/bs';
+import {MdLocationOn, MdEmail} from 'react-icons/md';
+
+const Footer = () => {
+    const contact_text1 = 'PUP Sta. Mesa, Manila, Philippines'
+    const contact_text2 = 'buildityourself@mail.com'
+    const contact_text3 = 'www.facebook.com/b-i-y'
+    const contact_text4 = 'www.intagram.com/@b-i-y'
+    const contact_text5 = 'www.youtube.com/Build-It-Yourself'
+
+    return (
+        <div className='homeFooter'> 
+            <div className='homeFooterleft'>
+                <h1>Social Media Links</h1>
+                <div className='contact_apps'> 
+                    <BsFacebook size={30}/> {contact_text3}
+                </div>
+                <div className='contact_apps'> 
+                    <BsInstagram size={30}/> {contact_text4}
+                </div>
+                <div className='contact_apps'> 
+                    <BsYoutube size={30}/> {contact_text5}
+                </div>
+            </div>
+            <div className='homeFooterright'>
+                <h1>Contact Info</h1>
+                <div className='contact_apps'>
+                    <MdLocationOn size={30}/>{contact_text1}
+                </div>
+                <div className='contact_apps'>
+                    <MdEmail size={30}/>{contact_text2}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
